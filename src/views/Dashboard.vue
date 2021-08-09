aef31a0b06a39883affd4db62ec5614da55e56ba
caneto123 <75550601+caneto123@users.noreply.github.com>
2021-08-09 20:27:24 -0300

    Log out
    logOut with firebase.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,63 @@
 <template>
-  <div>
+  <div class="vue-tempalte">
     <h2>Dashboard</h2>
     <p>This page is only visible to users that are currently logged in</p>
+    <h3>Welcome</h3>
+    <p>{{ user.displayName }}</p>
+    <p>{{ user.email }}</p>
+
+    <div class="my-5">
+      <v-btn
+        class="btnOne"
+        elevation="2"
+        color="#ea9f77"
+        type="submit"
+        @click="logOut()"
+      >
+        <v-icon left color="white"> mdi-login </v-icon>
+        <span class="white--text">Log out</span>
+      </v-btn>
+    </div>
+
+    <!-- <button
+      type="submit"
+      class="btn btn-dark btn-lg btn-block"
+      @click="logOut()"
+    >
+      Log out
+    </button> -->
   </div>
 </template>
+
+<script>
+import firebase from "firebase";
+
+export default {
+  data() {
+    return {
+      user: null,
+    };
+  },
+  created() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.user = user;
+      } else {
+        this.user = null;
+      }
+    });
+  },
+  methods: {
+    logOut() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          firebase.auth().onAuthStateChanged(() => {
+            this.$router.push("/login");
+          });
+        });
+    },
+  },
+};
+</script>
